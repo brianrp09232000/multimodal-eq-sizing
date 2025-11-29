c3ad49543fc64dc55d4d3ae07c198b9986ec62a8
yannidaaa <145985909+yannidaaa@users.noreply.github.com>
2025-11-28 18:31:13 -0600

    Add log market cap and SPY return calculations
    Implemented functions to compute log market capitalization and SPY returns.

--- src/data/features/price_features.py
+++ src/data/features/price_features.py
@@ -40,4 +40,71 @@ def compute_momentum_rank(input_df):
     # Merge 12-1 momentum and rank into the input dataset
     output_df = pd.merge(input_df, df2, on =['ticker','Date'], how='left')
 
-    return output_df# price_features.py – TODO: implement
+    return output_df
+
+def get_log_mktcap(input_df):
+    """
+    Compute log(market capitalization) for each ticker.
+    log(market cap_t) = log(Close_t) * SharesOutstanding. 
+    * Note that Yahoo only provides the latest shares outstanding, so we don't have the historical 
+    shares outstanding at time t. The calculation method for market capitalization may not be precise.
+    * In addition, some tickers may not have SharesOutstanding information in yfinance.
+    ----------
+    Input dataset must contain columns ['ticker', 'Date']; 
+    ----------
+    Output dataset adds additional columns:['log_mktcap']
+    """
+    start = input_df['Date'].min() 
+    end = input_df['Date'].max()
+    tickers = list(input_df['ticker'].unique())
+    
+    rows = []
+    for ticker in tickers:
+        stock = yf.Ticker(ticker)
+
+        # Get CURRENT shares outstanding
+        shares = stock.info.get("sharesOutstanding", None)
+
+        # Fetch historical price
+        df_price = stock.history(start=start, end=end)
+
+        # Add ticker column
+        df_price["ticker"] = ticker
+        df_price = df_price.reset_index()
+
+        # Compute market cap and log market cap
+        if shares is not None:
+            df_price["mktcap"] = df_price["Close"] * shares
+            df_price["log_mktcap"] = (df_price["mktcap"]).apply(
+                lambda x: None if pd.isna(x) else np.log(x)
+            )
+        else:
+            df_price["market_cap"] = None
+            df_price["log_mktcap"] = None
+
+        rows.append(df_price)
+    # concat all tickers and merge with the input dataset    
+    mktcap = pd.concat(rows, ignore_index=True)[['Date','ticker','log_mktcap']]
+    output_df = pd.merge(input_df, mktcap, on=['Date','ticker'], how='left')
+    
+    return output_df
+    
+def compute_SPY_r1 (input_df):
+    """
+    Compute SPY r1 = (Close_t/Clost_t_1) -1
+    ----------
+    Input dataset must contain columns ['Date']. 
+    ----------
+    Output dataset adds additional columns:['spy_r1']
+    """
+    start = input_df['Date'].min() 
+    end = input_df['Date'].max()
+    buffer_start = start - timedelta(days=7)
+    
+    df_spy = get_single_ticker_history("SPY", buffer_start, end)
+    df_spy["spy_r1"] = df_spy["Close"] / df_spy["Close"].shift(1) - 1
+    df_spy = df_spy[df_spy['Date']>=start]
+    df_spy = df_spy[['Date','ticker','spy_r1']]
+    
+    output_df = pd.merge(input_df, df_spy, on=['Date'], how='left')
+    return output_df
