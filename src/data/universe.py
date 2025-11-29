f2b0f0ea692cd207a739807804e15534147cd018
LauraSwafford <lcs15@rice.edu>
2025-11-29 14:41:05 -0600

    Update universe.py
    changed function output from data to list. now the function outputs a list of tickers with the most headlines rather than a dataframe.  Also, added optional start and stop dates as inputs to the function.

--- src/data/universe.py
+++ src/data/universe.py
@@ -3,19 +3,20 @@ import pandas as pd
 from src.data.features.news_features import count_headlines_per_ticker
 
 
-def tickers_with_most_headlines(news_df, n=200):
+def tickers_with_most_headlines(news_df, start=None, end=None, n=200):
     """Finds the tickers with the most headlines 
     Input: news_df pandas dataframe with ticker column for ticker symbols
             optional: n interger, number of top tickers to return
-    Output: pandas dataframe containing the number of headlines per ticker
+    Output: list containing the number of headlines per ticker
                 for the tickers with the most headlines"""
-
+    
     #count headlines for each ticker
-    df = count_headlines_per_ticker(news_df)
+    df = count_headlines_per_ticker(news_df, start, end)
 
     #limit dataframe to n tickers
     df = df.sort_values(['count'], ascending=False)
     df = df[:n]
     df.reset_index(drop=True, inplace=True)
     
-    return df
+    return list(df['ticker'])
+    
