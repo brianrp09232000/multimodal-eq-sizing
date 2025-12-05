f6daf659c61d1a8ecbb71a8b39b535bd1a84298f
LauraSwafford <lcs15@rice.edu>
2025-12-05 13:09:28 -0600

    Update price_mlp.py
    Added simple_rnn_price_model for leg 1 modeling

--- src/models/price_mlp.py
+++ src/models/price_mlp.py
@@ -1,1 +1,17 @@
 # price_mlp.py – TODO: implement
+import numpy as np
+
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Dense, SimpleRNN
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.optimizers import Adam
+
+def simple_rnn_price_model(model_shape):
+    model = Sequential()
+    model.add(SimpleRNN(units=50, return_sequences=True, input_shape=model_shape))
+    model.add(SimpleRNN(units=50, return_sequences=True))
+    model.add(SimpleRNN(units=50, return_sequences=False))
+    model.add(Dense(units=1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+  
