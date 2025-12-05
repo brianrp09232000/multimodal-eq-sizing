dd99df5bec34ea2a979be74a45982dcb23be3bae
LauraSwafford <lcs15@rice.edu>
2025-12-05 23:46:10 +0000

    updated price model notebook

--- src/models/price_mlp.py
+++ src/models/price_mlp.py
@@ -5,6 +5,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.layers import Dense, SimpleRNN
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import BatchNormalization, Dropout, Flatten, GRU
 
 def simple_rnn_price_model(model_shape):
     model = Sequential()
@@ -13,5 +14,17 @@ def simple_rnn_price_model(model_shape):
     model.add(SimpleRNN(units=50, return_sequences=False))
     model.add(Dense(units=1))
     model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+  
+
+
+def gru_price_model(model_shape):
+    model = Sequential()
+    model.add(GRU(units=128, dropout=0.2, activity_regularizer='l2', return_sequences=True,
+              input_shape=model_shape))
+    model.add(GRU(units=64, dropout=0.2, activity_regularizer='l2', return_sequences=True,
+              input_shape=model_shape))
+    model.add(GRU(units=32, dropout=0.2, activity_regularizer='l2'))
+    model.add(Dense(1))
     return model
   
